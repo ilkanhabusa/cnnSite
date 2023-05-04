901bec92c9190c39cc098f9ec2ac1bfa5cb40088
ilkanhabusa <122947619+ilkanhabusa@users.noreply.github.com>
2023-05-04 18:43:33 -0400

    Adding all the files

---
+++ js/master.js
@@ -0,0 +1,9 @@
+let decision = confirm("Press a button!\nEither OK or Cancel.");
+
+alert("the value of my variable decision is:"+ decision);
+
+if (decision) {
+    alert("I clicked OK");
+} else {
+    alert("I clicked Cancel");
+}
